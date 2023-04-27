269fde605c73f91415d4896153939373d1ce24b0
DPM <dylan.phillip.meyers@gmail.com>
2023-04-27 21:24:15 +0900

    receipts

--- Receipts.py
+++ Receipts.py
@@ -5,7 +5,6 @@ Lovely Loveseat. Tufted polyester blend on wood. 32 inches high x 40 inches wide
 """
 lovely_loveseat_price = 254.00
 
-
 stylish_settee_description = """
 Stylish Settee. Faux leather on birch. 29.50 inches high x 54.75 inches wide x 28 inches deep. Black.
 """
